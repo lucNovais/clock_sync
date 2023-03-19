162907ba8c32d4ba755da8013a9e4b453cc37081
lucnovais <lucas.novais@indicium.tech>
2023-03-19 10:55:32 -0300

    Updating print texts

--- src/client.py
+++ src/client.py
@@ -5,14 +5,14 @@ from constants import *
 def connect_to_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((SERVER_ADDRESS, SERVER_PORT))
-    print('Connected to server.')
+    print('Conectado ao servidor de tempo!\n')
 
     try:
         while True:
             data = sock.recv(BUFFER_SIZE).decode()
-            print('Received time from server:', data)
+            print('Tempo recebido do servidor:', data)
     except:
-        print('Connection closed by server.')
+        print('Conexao fechada pelo servidor!')
         sock.close()
 
 if __name__ == '__main__':
--- src/clock_server.py
+++ src/clock_server.py
@@ -4,31 +4,32 @@ import time
 
 from constants import *
 
-def get_ntp_time():
-    client = ntplib.NTPClient()
-    response = client.request(NTP_SERVER, version=3)
+def update_with_ntp():
+    ntp_client = ntplib.NTPClient()
+    response = ntp_client.request(NTP_SERVER, version=3)
     return response.tx_time
 
-def start_server():
+def start():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((SERVER_ADDRESS, SERVER_PORT))
     sock.listen(1)
-    print('Server started.')
+
+    print(f'Servidor de tempo iniciado!\nEscutando no endereco: {SERVER_ADDRESS}:{SERVER_PORT}\n')
 
     while True:
         conn, addr = sock.accept()
-        print('Client connected:', addr)
+        print(f'Cliente connectado: {addr}')
 
         try:
             while True:
-                ntp_time = get_ntp_time()
-                local_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ntp_time))
+                ntp_time = update_with_ntp()
+                local_time = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(ntp_time))
                 conn.sendall(local_time.encode())
-                print('Sent time to client:', local_time)
+                print(f'Tempo enviado para o cliente: {local_time}')
                 time.sleep(1)
         except:
-            print('Connection closed by client.')
+            print('Conexao encerrada pelo cliente!')
             conn.close()
 
 if __name__ == '__main__':
-    start_server()
+    start()

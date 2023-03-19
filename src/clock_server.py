31a7bde6b7e5211c957268243d514f6447dda534
lucnovais <lucas.novais@indicium.tech>
2023-03-19 12:28:20 -0300

    Adding threading to the server side

--- src/clock_server.py
+++ src/clock_server.py
@@ -1,6 +1,7 @@
 import ntplib
 import socket
 import time
+import threading
 
 from constants import *
 
@@ -8,6 +9,23 @@ def update_with_ntp():
     ntp_client = ntplib.NTPClient()
     response = ntp_client.request(NTP_SERVER, version=3)
     return response.tx_time
+
+def send_time(conn):
+    connected = True
+    while connected:
+        msg = conn.recv(BUFFER_SIZE).decode()
+
+        if msg == TIME_REQUEST:
+            try:
+                ntp_time = update_with_ntp()
+                local_time = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(ntp_time))
+                conn.sendall(local_time.encode())
+                print(f'Tempo enviado para o cliente: {local_time}')
+            except:
+                print('Conexao encerrada pelo cliente ou parada forcada!')
+                conn.close()
+                connected = False
+    exit()
 
 def start():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,17 +38,12 @@ def start():
         conn, addr = sock.accept()
         print(f'Cliente connectado: {addr}')
 
-        msg = conn.recv(BUFFER_SIZE).decode()
+        thread = threading.Thread(
+            target=send_time,
+            args=(conn)
+        )
 
-        if msg == TIME_REQUEST:
-            try:
-                ntp_time = update_with_ntp()
-                local_time = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(ntp_time))
-                conn.sendall(local_time.encode())
-                print(f'Tempo enviado para o cliente: {local_time}')
-            except:
-                print('Conexao encerrada pelo cliente ou parada forcada!')
-                conn.close()
+        thread.start()
 
 if __name__ == '__main__':
     start()
